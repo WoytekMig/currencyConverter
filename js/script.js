4deb67b109a4dbe0aa0c703d6c7a6837ed867264
Wojciech Migdal <wojciechjanmigdal@gmail.com>
2023-01-19 00:07:25 +0100

    making JS more clear

--- js/script.js
+++ js/script.js
@@ -1,9 +1,100 @@
 {
-const currencyHeldElement = document.querySelector('.js-currencyHeld');
-const moneyHeldElement = document.querySelector('.js-moneyHeld');
-const currencyWantedElement = document.querySelector('.js-currencyWanted');
-const formElement = document.querySelector('.js-form');
-const resultElement = document.querySelector('.js-result');
+    const helloWorld = () => {
+        console.log("Witam na zapleczu 😄!");
+    }
+
+    const currencyHeldElement = document.querySelector('.js-currencyHeld');
+    const moneyHeldElement = document.querySelector('.js-moneyHeld');
+    const currencyWantedElement = document.querySelector('.js-currencyWanted');
+    const formElement = document.querySelector('.js-form');
+    const resultElement = document.querySelector('.js-result');
+
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const moneyHeld = moneyHeldElement.value;
+        const currencyHeld = currencyHeldElement.value;
+        const currencyWanted = currencyWantedElement.value;
+
+        let currencyHeldRate;
+        switch (currencyHeld) {
+            case "EUR":
+                currencyHeldRate = 4.697;
+                break;
+            case "GBP":
+                currencyHeldRate = 5.351;
+                break;
+            case "USD":
+                currencyHeldRate = 4.401;
+                break;
+        }
+
+        let currencyWantedRate;
+        switch (currencyWanted) {
+            case "PLN":
+                currencyWantedRate = 1;
+                break;
+            case "EUR":
+                currencyWantedRate = 4.697;
+                break;
+            case "GBP":
+                currencyWantedRate = 5.351;
+                break;
+            case "USD":
+                currencyWantedRate = 4.401;
+                break;
+        }
+
+        if (currencyHeld == "PLN") {
+            result = moneyHeld / currencyWantedRate;
+        } else if (currencyHeld == "EUR" && (currencyWanted == "EUR" || currencyWanted == "PLN")) {
+            switch (currencyWanted) {
+            case "EUR" :
+                result = moneyHeld * 1;
+                break;
+           case "PLN" :
+                result = moneyHeld * currencyHeldRate;
+                break;
+            }
+        } else if (currencyHeld == "GBP" && (currencyWanted == "GBP" || currencyWanted == "PLN")) {
+            switch (currencyWanted) {
+            case "GBP" :
+                result = moneyHeld * 1;
+                break;
+            case "PLN" :
+                result = moneyHeld * currencyHeldRate;
+                break;
+            }
+        } else if (currencyHeld == "USD" && (currencyWanted == "USD" || currencyWanted == "PLN")) {
+            switch (currencyWanted) {
+            case "USD":
+                result = moneyHeld * 1;
+                break;
+            case "PLN":
+                result = moneyHeld * currencyHeldRate;
+                break;
+            }
+        } else {
+            result = moneyHeld * currencyHeldRate / currencyWantedRate;
+        }
+    
+        resultElement.innerText = `${result.toFixed(2)} ${currencyWanted}`;
+    });
+
+const init = () => {
+
+    helloWorld();
+};
+init();
+
+}
+
+
+
+
+/* 
+
+ const calculateResult = (currencyHeld, currencyHeldRate, currencyWanted, currencyWantedRate, moneyHeld)
 
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -62,9 +153,8 @@ formElement.addEventListener("submit", (event) => {
             result = moneyHeld * currencyHeldRate;
         }
     } else {
-        result = moneyHeld * currencyWantedRate / currencyHeldRate;
+        result = moneyHeld * currencyHeldRate / currencyWantedRate;
     }
 
     resultElement.innerText = `${result.toFixed(2)} ${currencyWanted}`;
-   });
-}
+}); */
